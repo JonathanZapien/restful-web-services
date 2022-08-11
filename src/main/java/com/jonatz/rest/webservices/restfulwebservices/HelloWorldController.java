2ae27e97357df1636f91ac59653f39d6cce46542
Jonathan Zapien <jonathan_zapien@epam.com>
2022-08-11 16:03:39 -0500

    [ADD] PathVariable annotation example added.

--- src/main/java/com/jonatz/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/jonatz/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,18 +1,25 @@
 package com.jonatz.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
   @GetMapping(path = "/hello-world")
-  public String helloWorld(){
+  public String helloWorld() {
     return "Hello World";
   }
 
   @GetMapping(path = "/hello-world-bean")
-  public HelloWorldBean helloWorldBean(){
+  public HelloWorldBean helloWorldBean() {
     return new HelloWorldBean("Hello World form Bean");
+  }
+
+  /// hello-world/path-variable/Jonathan
+  @GetMapping(path = "/hello-world/path-variable/{name}")
+  public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+    return new HelloWorldBean(String.format("Hello World, %s", name));
   }
 }
