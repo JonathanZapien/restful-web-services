2610f3d19dbb829abc758af0f5909cb39ef6296d
Jonathan Zapien <jonathan_zapien@epam.com>
2022-08-12 12:35:25 -0500

    [ADD] Simple POST method implementation for User

--- src/main/java/com/jonatz/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/jonatz/rest/webservices/restfulwebservices/user/UserResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +25,13 @@ public class UserResource {
   @GetMapping("/users/{id}")
   public User retrieveUser(@PathVariable int id) {
     return service.findOne(id);
+  }
+
+  // CREATED
+  // input - details of user
+  // output - CREATED and Return the created URI
+  @PostMapping("/users")
+  public void createUser(@RequestBody User user) {
+    User savedUser = service.save(user);
   }
 }
