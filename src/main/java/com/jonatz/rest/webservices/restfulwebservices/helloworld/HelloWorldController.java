c8a1aa9ecc816925279b08be87fd2ca47f6d2c65
Jonathan Zapien <jonathan_zapien@epam.com>
2022-08-15 18:09:38 -0500

    [ADD] i18n implemented for /good-morning-internationalized

--- src/main/java/com/jonatz/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/jonatz/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.jonatz.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+  /*
+   * This is used by Spring to pickup properties from message bundles.
+   * Springboot automatically make it work if you have a message.properties file
+   * */
+  @Autowired private MessageSource messageSource;
 
   @GetMapping(path = "/hello-world")
   public String helloWorld() {
@@ -17,9 +26,19 @@ public class HelloWorldController {
     return new HelloWorldBean("Hello World form Bean");
   }
 
-  /// hello-world/path-variable/Jonathan
   @GetMapping(path = "/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
+  }
+
+  @GetMapping(path = "/good-morning-internationalized")
+  public String goodMorningInternationalized(
+      // Instead of telling each method to use locale
+      // @RequestHeader(name = "Accept-Language", required = false) Locale locale
+      ) {
+
+    //    return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+    return messageSource.getMessage(
+        "good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
   }
 }
