731b0483d91a4aea27f84d8712a82b088b644c57
Jonathan Zapien <j.j.zapien@gmail.com>
2023-06-02 13:10:26 -0600

    [INIT] - Initial project setup - Initial project configuration which includes a sample controller with 3 different get calls.

---
+++ src/main/java/com/jonatz/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package com.jonatz.rest.webservices.restfulwebservices.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldBean {
+
+  private String message;
+
+}
