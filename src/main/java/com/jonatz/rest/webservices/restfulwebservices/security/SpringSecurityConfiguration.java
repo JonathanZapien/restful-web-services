f74e08519ecbfd273639bdb1eb847cbe35dfdb47
Jonathan Zapien <j.j.zapien@gmail.com>
2023-08-24 12:48:40 -0600

    [Feat] - Spring Security Implementation
    * SpringSecurityConfiguration - Intercept default behavior of spring security by using filterChain.

---
+++ src/main/java/com/jonatz/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.jonatz.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //All requests should be authenticated
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        //If a request is not authenticated, a web page is shown
+        http.httpBasic(withDefaults());
+        //CSRF -> POST, PUT
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
